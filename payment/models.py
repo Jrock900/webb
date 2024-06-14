8efd00ca92184f921a71a89266697c6fccbd51eb
tooturntjroock <jonathanw1219@gmail.com>
2024-06-13 19:24:42 -0500

    customers models

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from products.models import Product
 
 
 # Create your models here.
@@ -29,3 +29,33 @@ def create_shipping(sender, instance, created, **kwargs):
         user_shipping.save()
 
 post_save.connect(create_shipping,sender=User)
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=1500)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_order = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'
+
+
+
